e8eddc83a1ddbddb894daf9c5e282ae06bd35117
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 17:16:17 +0000

    Adding tarfile member sanitization to extractall()

--- Atomic/syscontainers.py
+++ Atomic/syscontainers.py
@@ -2431,7 +2431,26 @@ Warning: You may want to modify `%s` before starting the service""" % os.path.jo
         temp_dir = tempfile.mkdtemp()
         try:
             with tarfile.open(tarpath, 'r') as t:
-                t.extractall(temp_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(t, temp_dir)
                 manifest_file = os.path.join(temp_dir, "manifest.json")
                 if os.path.exists(manifest_file):
                     manifest = ""
